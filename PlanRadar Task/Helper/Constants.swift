d10bb7fb0f2cbb819ab37d686d58b5c395194057
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-04-17 12:35:43 +0200

    Add constants file

---
+++ PlanRadar Task/Helper/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  PlanRadar Task
+//
+//  Created by Walid Ahmed on 17/04/2025.
+//
+
+class Constants{
+    static var BASE_URL = "https://api.openweathermap.org/data/2.5/"
+    static var WEATHER = BASE_URL + "weather"
+}
