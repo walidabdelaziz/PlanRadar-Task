ef6f6bf1c9ec558a29068756b637eb3df675ecbb
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-04-17 14:04:26 +0200

    Add keyboard manager package

--- PlanRadar Task/AppDelegate.swift
+++ PlanRadar Task/AppDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // keyboard
+        IQKeyboardManager.shared.isEnabled = true
+        IQKeyboardManager.shared.resignOnTouchOutside = true
         return true
     }
 }
